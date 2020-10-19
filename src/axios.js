b5033371e8af73b930adc6cdf5cf4ec97bd0b1d1
MechaMic38 <69167202+MechaMic38@users.noreply.github.com>
2020-10-19 18:23:18 +0200

    Added stripe payment system (implemented with Node 8, expecting this to last until March 2021, then new solution will be required)

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: "http://localhost:5001/challenge-f0ac9/us-central1/api",
-  // "http://localhost:5001/challenge-f0ac9/us-central1/api",
+  baseURL: "https://us-central1-challenge-f0ac9.cloudfunctions.net/api",
 });
+
+// https://us-central1-challenge-f0ac9.cloudfunctions.net/api
+// http://localhost:5001/challenge-f0ac9/us-central1/api
 
 export default instance;
